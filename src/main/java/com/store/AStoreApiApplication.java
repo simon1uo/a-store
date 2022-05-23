9ed128071ba317b3b09394d4778a3ca6edecb163
Simon Luo <simonlaw910@gmail.com>
2022-05-23 14:17:49 +0800

    init: initialize project

---
+++ src/main/java/com/store/AStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AStoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/store/AStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
