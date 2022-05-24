7fea0b6ec6ff86e876558248ecabebff224d6bae
Simon Luo <simonlaw910@gmail.com>
2022-05-24 23:48:37 +0800

    feat: add global header navigation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router"
+import GlobalHeaderNav from "@/components/GlobalHeaderNav/GlobalHeaderNav.vue"
 </script>
 
 <template>
+    <GlobalHeaderNav />
     <RouterView />
 </template>
 
