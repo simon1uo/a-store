6e9c0939934bc75dd0765edd4e5d0ac1c322dfb4
Simon Luo <simonlaw910@gmail.com>
2022-05-25 13:27:44 +0800

    feat: add shopping bag related limit

--- src/main/java/com/store/constant/CommonConstant.java
+++ src/main/java/com/store/constant/CommonConstant.java
@@ -3,4 +3,9 @@ package com.store.constant;
 public class CommonConstant {
     public final static int TOKEN_LENGTH = 32;//token字段长度
 
+    public final static int SHOPPING_BAG_ITEM_LIMIT_NUMBER = 20;// 购物车中商品的最大数量
+
+    public final static int SHOPPING_BAG_ITEM_TOTAL_LIMIT_NUMBER = 20;// 购物车中商品的最大数量
+
+
 }
