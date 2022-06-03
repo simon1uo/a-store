f865ba9bd7be5c61df4bfdce1e45fb14a21056dc
Simon Luo <simonlaw910@gmail.com>
2022-06-03 15:22:01 +0800

    feat: add more info about user

--- src/main/java/com/store/controller/store/vo/AStoreUserInfoVO.java
+++ src/main/java/com/store/controller/store/vo/AStoreUserInfoVO.java
@@ -10,5 +10,14 @@ public class AStoreUserInfoVO implements Serializable {
     private static final long serialVersionUID = -7381622125813158664L;
 
     private String userAccount;
+
+    private String userPassword;
+
     private String userName;
+
+    private String userAvatarUrl;
+
+    private String userEmail;
+
+    private String userPhone;
 }
